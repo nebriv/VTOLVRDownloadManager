82d25ff6e60af208dab497e5b49168ba156dce23
nebriv <ben@benvirgilio.com>
2019-04-15 07:10:08 -0400

    Fixed self update check.

--- check_updates.py
+++ check_updates.py
@@ -12,7 +12,7 @@ def check_updates(cur_version, url=update_url):
         if "tag_name" in release_info:
             if LooseVersion(cur_version) < LooseVersion(release_info['tag_name']):
                 for each in release_info['assets']:
-                    if each['name'] == "VTOLVRMissionDownloader.exe":
+                    if each['name'] == "VTOLVRDownloader.exe":
                         return {"version": release_info['tag_name'], "browse_url": release_info['html_url'], "download_url": each['browser_download_url']}
     return False
 
